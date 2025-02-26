3aa77534f3af6a7a947167a76f3a3efc0190180b
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2025-02-25 21:02:03 -0800

    Update generate-msvc-files.bat

--- generate-msvc-files.bat
+++ generate-msvc-files.bat
@@ -55,15 +55,22 @@ echo.
 echo ========================== leveldb
 echo.
 
-set "MAKE_FLAGS=/I..\snappy /I..\snappy\msvc /I..\zstd\lib /DHAVE_SNAPPY /DHAVE_ZSTD" 
-::  /link ..\snappy\msvc\Debug\snappy.lib ..\zstd\msvc\lib\Debug\zstd_static.lib
-rmdir /s /q msvc >nul 2>&1
-mkdir msvc
-cd msvc
-cmake -G "Visual Studio 17" -A Win32 -DCMAKE_C_FLAGS="%MAKE_FLAGS%" -DCMAKE_CXX_FLAGS="%MAKE_FLAGS%" ..
-cd ..
-if errorlevel 1 goto :eof
-if not errorlevel 1 (
-    echo.
-    echo Open 'start msvc\leveldb.sln'
+if exist msvc (
+    echo To rebuild, delete the %CD%\msvc directory
 )
+
+if not exist msvc (
+    set "MAKE_FLAGS=/I..\snappy /I..\snappy\msvc /I..\zstd\lib /DHAVE_SNAPPY /DHAVE_ZSTD" 
+    rem  /link ..\snappy\msvc\Debug\snappy.lib ..\zstd\msvc\lib\Debug\zstd_static.lib
+    mkdir msvc
+    pushd msvc
+    cmake -G "Visual Studio 17" -A Win32 -DCMAKE_C_FLAGS="%MAKE_FLAGS%" -DCMAKE_CXX_FLAGS="%MAKE_FLAGS%" ..
+    msbuild leveldb.sln  /p:Configuration=Debug
+    popd
+)
+if errorlevel 1 goto :eof
+
+echo.
+echo.
+echo Run 'start msvc\leveldb.sln'
+echo.
